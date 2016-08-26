d22625d7a8671d4f276e4e24354c4c917ee589ab
tundish <tundish@thuswise.org>
2016-08-26 19:02:20 +0100

    Added setup and packaging files.

---
+++ README.rst
@@ -0,0 +1,80 @@
+..  Titling
+    ##++::==~~--''``
+
+Turberfield is a framework for discrete event simulations and turn-based
+games.
+
+It is developed by a private individual but released to the public under the
+`GNU General Public License`_.
+
+The API may change as the project proceeds. At this early stage you should
+only use the latest release; it may not be compatible with previous
+versions.
+
+Requirements
+::::::::::::
+
+* Turberfield runs on Python 3.4 or later.
+* Docutils
+* Simpleaudio
+
+Usage
+:::::
+
+Download and unpack the source distribution::
+
+    $ tar -xzvf turberfield-dialogue-0.1.0.tar.gz
+    $ cd turberfield-dialogue-0.1.0
+
+Run the tests::
+
+    $ python3.4 -m unittest discover turberfield
+
+Consult the documentation::
+
+    $ firefox turberfield/dialogue/doc/html/index.html
+
+Installation
+::::::::::::
+
+::
+
+    $ pip install turberfield-dialogue
+
+Roadmap
+:::::::
+
+Turberfield is a *S* ur *PRISE*.
+
+**Semantic**
+    Turberfield can be understood by machines or human beings.
+
+**Persistent**
+    Turberfield can be stopped and saved for later.
+
+**Reusable**
+    Turberfield can be turned into something else.
+
+**Interactive**
+    Turberfield listens to what you say.
+
+**Simulation**
+    Turberfield knows what it's talking about.
+
+for **Economics**
+    ...or Education, or Entertainment. Turberfield is deadly serious.
+    And only a game.
+
+What's missing? *UR*!
+
+* If you've spotted a bug in Turberfield, please let me know so I can fix it.
+* If you think Turberfield lacks a feature, you can help drive development by describing
+  your Use Case.
+
+In either event, please leave a message on the project's `message board`_.
+
+:Author: tundish
+:Copyright: 2016 D Haynes
+
+.. _GNU General Public License: http://www.gnu.org/licenses/gpl.html
+.. _message board: https://www.assembla.com/spaces/turberfield/messages
---
+++ setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+import ast
+import os.path
+
+from setuptools import setup
+
+
+try:
+    # For setup.py install
+    from turberfield.dialogue import __version__ as version
+except ImportError:
+    # For pip installations
+    version = str(ast.literal_eval(
+                open(os.path.join(os.path.dirname(__file__),
+                "turberfield", "dialogue", "__init__.py"),
+                'r').read().split("=")[-1].strip()))
+
+__doc__ = open(os.path.join(os.path.dirname(__file__), "README.rst"),
+               'r').read()
+
+setup(
+    name="turberfield-dialogue",
+    version=version,
+    description="Scripting dialogue and soundtrack for your Turberfield project.",
+    author="D Haynes",
+    author_email="tundish@thuswise.org",
+    url="https://www.assembla.com/spaces/turberfield/messages",
+    long_description=__doc__,
+    classifiers=[
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: GNU General Public License v3"
+        " or later (GPLv3+)"
+    ],
+    namespace_packages=["turberfield"],
+    packages=[
+        "turberfield.dialogue",
+        "turberfield.dialogue.test",
+    ],
+    package_data={
+        "turberfield.dialogue": [
+            "doc/*.rst",
+            "doc/_templates/*.css",
+            "doc/html/*.html",
+            "doc/html/*.js",
+            "doc/html/_sources/*",
+            "doc/html/_static/css/*",
+            "doc/html/_static/font/*",
+            "doc/html/_static/js/*",
+            "doc/html/_static/*.css",
+            "doc/html/_static/*.gif",
+            "doc/html/_static/*.js",
+            "doc/html/_static/*.png",
+            ],
+    },
+    install_requires=[
+        "docutils>=0.12",
+        "simpleaudio>=1.0.1",
+        ],
+    extras_require={
+        "dev": [
+            "pep8>=1.6.2",
+        ],
+        "docbuild": [
+            "babel>=2.2.0",
+            "sphinx-argparse>=0.1.15",
+            "sphinxcontrib-seqdiag>=0.8.4",
+            "sphinx_rtd_theme>=0.1.9"
+        ],
+    },
+    tests_require=[],
+    entry_points={
+        "console_scripts": [],
+    },
+    zip_safe=False
+)
