fe0af0374568e1c511d2ff6aa75073f4865f1853
Tundish <tundish@thuswise.org>
2016-10-15 17:12:36 +0100

    Test boilerplate.

--- turberfield/dialogue/test/test_battle.py
+++ turberfield/dialogue/test/test_battle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 #   -*- encoding: UTF-8 -*-
 
-# This file is part of Addison Arches.
+# This file is part of Turberfield Dialogue.
 #
-# Addison Arches is free software: you can redistribute it and/or modify it
+# Turberfield Dialogue is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Addison Arches is distributed in the hope that it will be useful, but
+# Turberfield Dialogue is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
+# along with Turberfield Dialogue.  If not, see <http://www.gnu.org/licenses/>.
 
 import textwrap
 import unittest
