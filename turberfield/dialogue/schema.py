a9cc243d98c4c833f824da5f0094daf9e5d87fba
Tundish <tundish@thuswise.org>
2017-03-10 11:30:00 +0000

    Added schema and tests.

---
+++ turberfield/dialogue/schema.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import OrderedDict
+import enum
+
+from turberfield.utils.db import Table
+from turberfield.utils.misc import gather_installed
+
+
+schema = OrderedDict(
+    (table.name, table) for table in [
+    Table(
+        "entity",
+        cols=[
+          Table.Column("id", int, True, False, False, None, None),
+          Table.Column("session", str, False, False, True, None, None),
+          Table.Column("name", str, False, False, True, None, None),
+        ]
+    ),
+    Table(
+        "state",
+        cols=[
+          Table.Column("id", int, True, False, False, None, None),
+          Table.Column("class", str, False, False, True, None, None),
+          Table.Column("name", str, False, False, True, None, None),
+          Table.Column("value", int, False, False, False, None, None),
+        ]
+    ),
+    Table(
+        "touch",
+        cols=[
+          Table.Column("sbjct", int, False, False, False, None, "entity"),
+          Table.Column("objct", int, False, True, False, None, "entity"),
+        ]
+    )
+])
+
+
+@enum.unique
+class Ownershipstate(enum.IntEnum):
+    lost = 0
+    acquired = 1
+
+
+@enum.unique
+class Visibility(enum.IntEnum):
+    invisible = 0
+    visible = 1
---
+++ turberfield/dialogue/test/test_schema.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import OrderedDict
+import datetime
+import unittest
+
+from turberfield.utils.db import Connection
+from turberfield.utils.db import Creation
+from turberfield.utils.db import Insertion
+from turberfield.utils.db import Table
+from turberfield.utils.misc import gather_installed
+from turberfield.dialogue.schema import schema
+from turberfield.utils.test.test_db import DBTests
+
+
+class SQLTests(unittest.TestCase):
+
+    def test_create_entity(self):
+        expected = "\n".join((
+            "create table if not exists entity(",
+            "id INTEGER PRIMARY KEY NOT NULL,",
+            "session TEXT  NOT NULL,",
+            "name TEXT  NOT NULL,",
+            "UNIQUE(session, name)",
+            ")"
+        ))
+        rv = Creation(schema["entity"]).sql
+        self.assertEqual(2, len(rv))
+        self.assertEqual(expected, rv[0])
+
+    def test_insert_entity(self):
+        expected = (
+            "insert into entity (session, name) values (:session, :name)",
+            {"session": "1234567890", "name": "qwerty"}
+        )
+        rv = Insertion(
+            schema["entity"],
+            data=dict(
+                session="1234567890",
+                name="qwerty"
+            )
+        ).sql
+        self.assertEqual(expected, rv)
+
+    def test_create_touch(self):
+        expected = "\n".join((
+            "create table if not exists touch(",
+            "sbjct INTEGER  NOT NULL,",
+            "objct INTEGER,",
+            "FOREIGN KEY (sbjct) REFERENCES entity(id)",
+            "FOREIGN KEY (objct) REFERENCES entity(id)",
+            ")"
+        ))
+        rv = Creation(schema["touch"]).sql
+        self.assertEqual(2, len(rv))
+        self.assertEqual(expected, rv[0])
+
+class DBTests:
+
+    @staticmethod
+    def get_tables(con):
+        cur = con.cursor()
+        try:
+            cur.execute("select * from sqlite_master where type='table'")
+            return [
+                OrderedDict([(k, v)
+                for k, v in zip(i.keys(), i)])
+                for i in cur.fetchall()
+            ]
+        finally:
+            cur.close()
+
+class TableTests(DBTests, unittest.TestCase):
+
+    def test_creation_sql(self):
+        table = Table(
+            "records",
+            cols=[
+              Table.Column("id", int, True, False, None, None, None),
+              Table.Column("ts", datetime.datetime, False, False, None, None, None),
+              Table.Column("valid", bool, False, True, None, None, None),
+              Table.Column("data", str, False, True, None, None, None),
+            ]
+        )
+        con = Connection(**Connection.options())
+        with con as db:
+            rv = Creation(table).run(db)
+            n = len(self.get_tables(db))
+            self.assertEqual(1, n)
+
+    def test_state_tables(self):
+        states = dict(gather_installed("turberfield.dialogue.states"))
+        con = Connection(**Connection.options())
+        with con as db:
+            rv = Creation(*schema.values()).run(db)
+            tables = self.get_tables(db)
+            self.assertIn("state", [t.get("name") for t in tables])
