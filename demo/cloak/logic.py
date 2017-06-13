1c8887051ea15d05825db16a46bbe8eba20b3fe9
Tundish <tundish@gigeconomy.org.uk>
2017-06-13 18:53:52 +0100

    Replicated Cloak game under local demo.

---
+++ demo/cloak/logic.py
@@ -0,0 +1,98 @@
+import enum
+from itertools import repeat
+import random
+
+from turberfield.dialogue.model import SceneScript
+from turberfield.dialogue.types import EnumFactory
+from turberfield.dialogue.types import DataObject
+from turberfield.dialogue.types import Stateful
+
+@enum.unique
+class Location(EnumFactory, enum.Enum):
+    foyer = 0
+    bar = 1
+    cloakroom_floor = 2
+    cloakroom = 3
+    cloakroom_hook = 4
+
+class Narrator(Stateful):
+    pass
+
+class Cloak(Stateful, DataObject):
+    pass
+
+class Prize(Stateful, DataObject):
+    pass
+
+
+ensemble = [
+    Narrator().set_state(Location.foyer),
+    Cloak().set_state(Location.foyer).set_state(1),
+    Prize(message="You win!")
+]
+
+
+def parse_command(cmd):
+    try:
+        return cmd.strip().split(" ")[-1][0].lower()
+    except:
+        return None
+
+
+def interaction(folder, index, ensemble, cmd="", log=None, loop=None):
+    narrator, cloak, prize, *others = ensemble
+    locn = narrator.get_state(Location)
+    action = None
+    if locn == Location.foyer:
+        while action not in ("s", "w", "q"):
+            action = parse_command(cmd or input("Enter a command: "))
+        if action == "s":
+            narrator.set_state(Location.bar)
+            if cloak.get_state(Location) == locn:
+                prize.set_state(0)
+            else:
+                prize.set_state(1)
+        elif action == "w":
+            narrator.set_state(Location.cloakroom)
+            cloak.set_state(1)
+        else:
+            return None
+    elif locn == Location.bar:
+        while action != "n":
+            action = parse_command(cmd or input("Enter a command: "))
+
+        narrator.set_state(Location.foyer)
+        prize.message = prize.message.replace(
+            random.choice(prize.message), " ", 1
+        )
+        prize.set_state(0)
+    elif locn == Location.cloakroom:
+        while action not in ("c", "h", "e"):
+            action = parse_command(cmd or input("Enter a command: "))
+        if action == "c":
+            if cloak.get_state(Location) == Location.cloakroom:
+                cloak.set_state(Location.cloakroom_floor)
+            else:
+                cloak.set_state(Location.cloakroom)
+        elif action == "h":
+            cloak.set_state(Location.cloakroom_hook)
+        else:
+            narrator.set_state(Location.foyer)
+            if cloak.get_state(Location) != locn:
+                cloak.set_state(0)
+
+    if cloak.get_state(Location) == locn:
+        cloak.set_state(narrator.get_state(Location))
+        cloak.set_state(1)
+
+    return folder
+
+references = ensemble + [Location]
+
+folder = SceneScript.Folder(
+    pkg=__name__,
+    description="The 'Hello World' of text games.",
+    metadata=None,
+    paths=["foyer.rst", "bar.rst", "cloakroom.rst"],
+    interludes=repeat(interaction)
+)
